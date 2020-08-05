b94d77e17302f7ea4cd027f63be183632f842f8f
PederHA <peder.andresen@gmail.com>
2020-08-05 12:58:29 +0200

    Add serialization functions

--- internal/utils/serialize.go
+++ internal/utils/serialize.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"encoding/json"
 	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
 )
 
 func MarshalJSON(path string, i interface{}) error {
@@ -10,11 +12,32 @@ func MarshalJSON(path string, i interface{}) error {
 	if err != nil {
 		return err
 	}
+	return ioutil.WriteFile(path, b, 0644)
+}
 
-	err = ioutil.WriteFile(path, b, 0644)
+// I'm not sure how robust this is? Param i MUST be a pointer.
+func UnmarshalJSON(path string, i interface{}) error {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, i)
+}
+
+func MarshalYAML(path string, i interface{}) error {
+	b, err := yaml.Marshal(i)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	return ioutil.WriteFile(path, b, 0644)
+}
+
+// I'm not sure how robust this is? Param i MUST be a pointer.
+func UnmarshalYAML(path string, i interface{}) error {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(b, i)
 }
