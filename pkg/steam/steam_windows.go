48ee7672cd363e76163535e54d0b396d5be91f0d
PederHA <peder.andresen@gmail.com>
2020-08-10 14:41:44 +0200

    Fix registry key potentially not being closed

--- pkg/steam/steam_windows.go
+++ pkg/steam/steam_windows.go
@@ -6,14 +6,15 @@ import "golang.org/x/sys/windows/registry"
 
 func Path() (string, error) {
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\WOW6432Node\Valve\Steam`, registry.QUERY_VALUE)
+	defer k.Close()
 	if err != nil {
 		return "", err
 	}
-	defer k.Close()
 
 	s, _, err := k.GetStringValue("InstallPath")
 	if err != nil {
 		return "", nil // NOTE: Should we not propagate this error?
 	}
+
 	return s, nil
 }
